ddda4c134a6448c3d32318da49497a2a2d69c491
Payal234 <payaldhobale399@gmail.com>
2026-02-27 12:00:45 +0530

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-10xl bg-yellow-600 font-bold text-white">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
